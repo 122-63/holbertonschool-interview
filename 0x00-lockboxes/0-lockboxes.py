ede69d27a766595b2a80ab1351318e0260241af0
122-63 <3469@holbertonschool.com>
2022-04-23 20:48:32 -0500

    True or false

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Lockboxes
 You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1 
+Each box is numbered sequentially from 0 to n - 1
 and each box may contain keys to the other boxes.
 Write a method that determines if all the boxes can be opened.
 """
+
+
 def canUnlockAll(boxes):
     """return True or False"""
     unlocked = [0]
@@ -17,6 +19,7 @@ def canUnlockAll(boxes):
     if len(unlocked) == len(boxes):
         return True
     return False
+
 
 """def canUnlockAll(boxes):
     return True or False
