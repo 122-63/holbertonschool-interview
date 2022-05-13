acc1ad1584e1e4b1954a2266065566a140c8da8c
122-63 <3469@holbertonschool.com>
2022-05-12 23:22:50 -0500

    Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+def minOperations(n):
+    """
+    calculates the fewest number of operations
+    needed to result in exactly n H characters
+    in the file.
+    """
+
+    minOpe = 0
+    div = 2
+    while isinstance(n, int) and n > 1:
+        while n % div:
+            div += 1
+        minOpe += div
+        n = int(n / div)
+    return minOpe
