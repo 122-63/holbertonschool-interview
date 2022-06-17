5feaebf9215fb4f9ac1133f0603aed9f9500292c
122-63 <3469@holbertonschool.com>
2022-06-16 22:09:28 -0500

    palindrome

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,65 +1,31 @@
 #include "palindrome.h"
-#include <stdio.h>
-
 
 /**
- * is_palindrome - Look if a number is palindrome
- * @n: number to be checked
+ * is_palindrome - checks if given unsigned int is a palindrome
+ * @n: unsigned long int to check if palindrome
  *
- * Return: 1 if n is a palindrome, and 0 otherwise
+ * Return: returns 1 if palindrome, 0 otherwise
  */
+
 int is_palindrome(unsigned long n)
 {
-	unsigned long length_all = length_number(n);
-	unsigned long left = 1, right = 1, l = 0, r = 1, i = 1;
-	unsigned long length_half = (length_all / 2), step = 0;
+	unsigned long original = n, reverse = 0;
+	int last = 0;
 
-	if (!length_all)
+	while (n != 0)
+	{
+		/* find the value of the last digit */
+		last = n % 10;
+		/* store the value in a reverse number */
+		/* multiply by ten to increase the digit count of reverse */
+		reverse *= 10;
+		/* add the last digit to reverse */
+		reverse += last;
+		/* cut off the last digit so the new last digit is next digit */
+		n /= 10;
+	}
+	/* check if original number and calculated reverse are the same */
+	if (original == reverse)
 		return (1);
-
-	/* Right Side: Count Zeros */
-	for (; i < length_all; ++i)
-	{
-		left *= 10;
-	}
-
-	/* Traversing in the numbers */
-	for (; step < length_half; ++step)
-	{
-		l = n / left;
-		l %= 10;
-
-		r = n / right;
-		r %= 10;
-
-		if (l != r)
-			return (0);
-
-		right *= 10;
-		left /= 10;
-	}
-
-	return (1);
-}
-
-
-
-/**
- * length_number - Look amount of numbers
- * @n: (unsigned long) Number to count
- *
- * Return: Length of number complete
- */
-int length_number(unsigned long n)
-{
-	int count = 0;
-	unsigned long i = 10, j = 1;
-
-	for (; j != 0; (i *= 10), ++count)
-	{
-		j = n / i;
-		j %= 10;
-	}
-
-	return (count);
+	return (0);
 }
--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -1,7 +1,16 @@
-#ifndef PALINDROME
-#define PALINDROME
+#ifndef PALINDROME_H
+#define PALINDROME_H
 
-int length_number(unsigned long n);
+/* INCLUDED LIBRARIES */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* FUNCTION PROTOTYPES */
+/* function to check if unsigned long int is a palindrome */
 int is_palindrome(unsigned long n);
+/* helper function to check if unsigned int is a palindrome recursively */
+int palindrome_check(unsigned long *n, int *digit);
+/* helper function to calculate place as power of 10 based on digit count */
+unsigned long power_of_ten(int digit);
 
 #endif
