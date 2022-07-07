d3fcf630d202b222c45f12c1e9176663e8b081ae
122-63 <3469@holbertonschool.com>
2022-07-07 00:00:35 -0500

    2D Menger sponge

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -1,44 +1,10 @@
-#include "menger.h"
+#ifndef MENGEL_XD
+#define MENGEL_XD
 
-/**
- * menger - Draw 2D Meng
- * @level: MLevel to draw
- */
-void menger(int level)
-{
-	int row, column, size;
-	char rizar;
+#include <stdio.h>
+#include <math.h>
 
-	size = pow(3, level);
+void menger(int level);
+char characters(int row, int column);
 
-	for (row = 0; row < size; ++row)
-	{
-		for (column = 0; column < size; ++column)
-		{
-			rizar = characters(row, column);
-			printf("%c", rizar);
-		}
-		putchar(10);
-	}
-}
-
-
-/**
- * characters - draws a character
- * @row: row
- * @column: cols
- * Return: (char) Characters
- */
-char characters(int row, int column)
-{
-	while (row || column)
-	{
-		if (row % 3 == 1 && column % 3 == 1)
-			return (' ');
-
-		row /= 3;
-		column /= 3;
-	}
-
-	return ('#');
-}
+#endif
