6ff6bc31e7574b12c5f4c9b26aa58a2a67c2bf9f
122-63 <3469@holbertonschool.com>
2022-07-07 19:16:39 -0500

    update

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,71 +1,55 @@
 #!/usr/bin/python3
-""" This is my problem :'v """
+"""
+    Log parsing module
+"""
 import sys
-import re
-import signal
-from collections import OrderedDict
 
 
-def search_items(line, s):
-    """ Search the items to positionate """
-    regexu = r"\s\d{3}\s\d{1,}"
-    txt = re.search(regexu, line)
-    word = txt.group()
-    word = word[1:]
-
-    regexd = r"\d{3}\s"
-    left = re.search(regexd, word)
-
-    code = left.group()
-    code = code[:-1]
-
-    regext = r"\s\d{1,}"
-    right = re.search(regext, word)
-
-    size = right.group()
-    size = size[1:]
-    size = int(size)
-
-    add_code(code, s)
-
-    return size
-
-
-def add_code(code, codes):
-    """ Count the status code """
-    try:
-        codes[code] += 1
-    except KeyError:
-        pass
-
-
-def print_all(stat):
-    """ Print all """
-    stat = OrderedDict(stat)
-
-    for key, value in stat.items():
-        if value is not 0:
-            print("{}: {}".format(key, value))
+def print_information(status_codes, size):
+    """ Prints log information.
+       Args:
+           status_codes (:obj:`str` of `int`): The status codes with its hit
+            count.
+           size (int): The file size.
+    """
+    print("File size: {:d}".format(size))
+    for code, count in sorted(status_codes.items()):
+        if count != 0:
+            print("{}: {:d}".format(code, count))
 
 
 if __name__ == "__main__":
-    status = {"200": 0, "301": 0, "400": 0, "401": 0,
-              "403": 0, "404": 0, "405": 0, "500": 0}
-    file_size = 0
-    i = 0
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0, "401": 0, "403": 0, "404": 0, "405": 0,
+        "500": 0
+    }
+    size = 0
+    counter = 0
 
     try:
-        for lines in sys.stdin:
-            file_size += search_items(lines, status)
+        for line in sys.stdin:
+            line = line.split()[::-1]
 
-            if i is not 0 and i % 9 == 0:
-                print("File size: {:d}".format(file_size))
-                print_all(status)
+            try:
+                size += int(line[0])
+            except:
+                pass
 
-            i += 1
+            try:
+                status = line[1]
+                if status in status_codes:
+                    status_codes[status] += 1
+            except:
+                pass
+
+            counter += 1
+            if counter == 10:
+                print_information(status_codes, size)
+                counter = 0
+        print_information(status_codes, size)
+
     except KeyboardInterrupt:
-        pass
-    finally:
-        print("File size: {:d}".format(file_size))
-        print_all(status)
-        sys.exit(0)
+        print_information(status_codes, size)
+        raise
